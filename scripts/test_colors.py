7cd452a2a4cff3803e516f10968e7a6cdb8abe7c
Ho Kyung Sung <hokyungsung@DN0a221131.SUNet>
2019-07-01 17:37:55 -0700

    Update from local

--- scripts/test_colors.py
+++ scripts/test_colors.py
@@ -104,6 +104,7 @@ if __name__ == '__main__':
     parser.add_argument('out_dir', type=str, help='where to store results from')
     parser.add_argument('--sup_lvl', type=float, help='supervision level, if any')
     parser.add_argument('--num_iter', type=int, default=1, help='number of total iterations performed on each setting [default: 1]')
+    parser.add_argument('--hard', action='store_true', help='whether the dataset is to be easy')
     parser.add_argument('--seed', type=int, default=42)
     args = parser.parse_args()
 
@@ -117,7 +118,7 @@ if __name__ == '__main__':
     for i in range(1, args.num_iter + 1):
         epoch, track_loss, sup_img, vocab, vocab_size = \
             load_checkpoint(folder=args.load_dir,
-                            filename='checkpoint_{}_{}_best.pth.tar'.format(args.sup_lvl, i))
+                            filename='checkpoint_{}_{}_best.pth.tar'.format(args.sup_lvl, i) + ('hard' if hard else ''))
         
         print("iteration {}".format(i))
         print("best training epoch: {}".format(epoch))
@@ -130,7 +131,7 @@ if __name__ == '__main__':
 
     losses = np.array(losses)
     accuracies = np.array(accuracies)
-    np.save(os.path.join(args.out_dir, 'accuracies_{}.npy'.format(args.sup_lvl)), accuracies)
+    np.save(os.path.join(args.out_dir, 'accuracies_{}.npy'.format(args.sup_lvl)) + ('hard' if hard else ''), accuracies)
 
     print()
     print("======> Average loss: {}".format(np.mean(losses)))
