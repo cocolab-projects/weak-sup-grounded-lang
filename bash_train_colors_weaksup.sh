054d0bc19faa81ff1f8aa3a77809c4da1c424260
Ho Kyung Sung <hokysung@node13-ccncluster.stanford.edu>
2019-07-01 17:32:35 -0700

    Update from cluster

--- bash_train_colors_weaksup.sh
+++ bash_train_colors_weaksup.sh
@@ -3,8 +3,8 @@ cd scripts
 OUT_DIR=$1;
 ACC_OUT_DIR=$2;
 
-for SUP_LEV in 0.0005 0.001 0.005 0.01 0.02 0.05 0.1 0.5 1
+for SUP_LEV in 0.0015 0.002 0.0025 0.003 0.004 0.02 0.2
 do
     screen -S colors_${SUP_LEV}_supervised -dm bash -c "CUDA_VISIBLE_DEVICES=8 python train_colors_weaksup.py ${OUT_DIR} ${SUP_LEV} --num_iter 3 --cuda;
-    python test_colors.py ${OUT_DIR} ${ACC_OUT_DIR} --sup_lvl ${SUP_LEV} --num_iter 3; exec bash";
+    CUDA_VISIBLE_DEVICES=8 python test_colors.py ${OUT_DIR} ${ACC_OUT_DIR} --sup_lvl ${SUP_LEV} --num_iter 3; exec bash";
 done
